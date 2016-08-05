fd01ce483ca92583d4036c454470a6e82f2e16b2
Tom Hinton <tom.hinton@cse.org.uk>
2016-08-05 15:00:29 +0100

    use taffybar/dbus not xmobar. xmobar wakes up all the time

--- .xmonad/lib/XMonad/Util/XMobar.hs
+++ .xmonad/lib/XMonad/Util/XMobar.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleContexts, OverloadedStrings #-}
 
 module XMonad.Util.XMobar where
 
@@ -6,23 +6,33 @@ import XMonad
 import XMonad.Util.Run
 import XMonad.Hooks.DynamicLog
 import Data.Char
+import DBus
+import DBus.Client
+import Codec.Binary.UTF8.String ( decodeString )
+import Graphics.UI.Gtk hiding ( Signal )
 
 runWithBar cfg =
-  do pipe <- spawnPipe "xmobar"
-     let pp = (myPP (focusedBorderColor cfg)) { ppOutput = hPutStrLn pipe }
+  do session <- connectSession
+     let sendBus s = emit session (signal"/org/xmonad/Log" "org.xmonad.Log" "Update")
+            {signalBody = [toVariant $ decodeString s]}
+         pp = (myPP (focusedBorderColor cfg)) { ppOutput = sendBus }
          log = dynamicLogWithPP pp
      xmonad $ cfg { logHook = (logHook cfg) >> log }
 
-raw s = wrap ("<raw=" ++ (show $ length s)++":") ("/>") s
+raw = escapeMarkup
 
-font n = wrap ("<fn=" ++ (show n) ++ ">") "</fn>"
+taffyBold = wrap "<b>" "</b>"
+
+taffyColor fg bg = wrap t "</span>"
+  where
+    t = concat ["<span fgcolor=\"", fg, if null bg then "" else "\" bgcolor=\"" ++ bg , "\">"]
 
 myPP c = xmobarPP
-  { ppCurrent = xmobarColor c "" . font 1 -- . (map toUpper)
-  , ppVisible = xmobarColor "white" "" . font 1
-  , ppUrgent = xmobarColor "red" ""
-  , ppTitle = xmobarColor "white" "" . raw . shorten 120
-  , ppLayout = \s -> xmobarColor "grey" "" $ case s of
+  { ppCurrent = taffyColor c "" . taffyBold
+  , ppVisible = taffyColor "white" "" . taffyBold
+  , ppUrgent = taffyColor "red" ""
+  , ppTitle = taffyColor "white" "" . raw . shorten 120
+  , ppLayout = \s -> taffyColor "grey" "" $ case s of
       "Full" -> "+"
       x:" by H" -> x:""
       x:" by Full" -> x:"+"
