f516be339909fb98bce4ede3f4384079bc33bfb0
Tom Hinton <t@larkery.com>
2016-12-01 23:06:00 +0000

    set border color blue when only window on ws

--- lib/Local/Manage.hs
+++ lib/Local/Manage.hs
@@ -13,20 +13,44 @@ import XMonad.Util.NamedWindows (getName)
 import qualified XMonad.StackSet as W
 import XMonad.Actions.CopyWindow (copyToAll)
 
+import qualified Debug.Trace as D
+
 setBorder c w = withDisplay $ \d -> io $ do
   g <- initColor d c
   whenJust g $ \g' -> setWindowBorder d w g'
+
+
+-- getWindowRect :: Window -> X Rectangle
+-- getWindowRect w = do d <- asks display
+--                      bw <- getWindowBorderWidth d w
+--                      atts <- io $ getWindowAttributes d w
+--                      return $ Rectangle
+--                        (fromIntegral $ wa_x atts)
+--                        (fromIntegral $ wa_y atts)
+--                        (fromIntegral $ bw + wa_width atts)
+--                        (fromIntegral $ bw + wa_height atts)
+
+setBorderHook =
+  let twoOrMore :: [a] -> Bool
+      twoOrMore (_:(_:_)) = True
+      twoOrMore _ = False in
+  withFocused $ \w -> do
+  us <- fmap (not . null) readUrgents
+--  rect <- getWindowRect w
+--  scr <- gets $ screenRect . W.screenDetail . W.current . windowset
+  others <- gets $ twoOrMore . W.integrate' . W.stack . W.workspace . W.current . windowset
+  -- this is no good as scr and rect are not right
+  -- what I want is to know if there are other windows that are invisible?
+
+  if us then setBorder Local.Theme.hasUrgentBorderColor w
+    else if (not others) then setBorder "blue" w
+    else return ()
 
 addManageRules c = withUrgencyHookC LibNotifyUrgencyHook
                    urgencyConfig { suppressWhen = Focused
                                  , remindWhen = Every 120 }
                    $ c { manageHook = (manageHook c) <+> windowRules
-                       , logHook = (logHook c) >>
-                                   (withFocused $ \w -> do
-                                       us <- fmap null readUrgents
-                                       when (not us) $ setBorder Local.Theme.hasUrgentBorderColor w)
-                                   -- it would be nice to have a fullscreen border color
-                       }
+                       , logHook = (logHook c) >> setBorderHook }
 
 windowRules = composeAll
   [ isDialog --> doFloat
