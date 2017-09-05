7d8a7f5e7888f148cde33893ff5154f1274aba65
Tom Hinton <tom.hinton@cse.org.uk>
2017-09-05 14:53:10 +0100

    minor colouring in

--- lib/Local/Colors.hs
+++ lib/Local/Colors.hs
@@ -1,6 +1,6 @@
 module Local.Colors where
 
-focusedBorderColor   = "#b8b8b8"
+focusedBorderColor   = "#c8c8c8"
 normalBorderColor    = "#404040"
 inactiveTabColor     = "#313131"
 otherWindow          = "#a8a8a8"
@@ -9,9 +9,9 @@ overflowWindow       = "#727272"
 singleSelectionColor = "#ffffff"
 multiSelectionColor  = "darkorange"
 
-focusedText          = "black"
+focusedText          = "#101010"
 normalText           = "white"
 urgentText           = "white"
 
-smallFont = "xft:Sans-8"
+smallFont = "xft:Sans-8:bold"
 bigFont = "xft:Sans-12"
